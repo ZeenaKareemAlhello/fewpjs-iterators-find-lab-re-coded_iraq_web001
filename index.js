afdf9e90f349d070df07baaa78b9cc575a68c8c9
zeena kareem <zena34269@gmail.com>
2020-06-16 18:20:10 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  
+]
+
+function superbowlWin(array){
+  let a
+
+  a=array.find((item)=> item.result==="W" )
+  if(typeof a !=='undefined'){
+   return  a.year}
+   else{
+   return undefined}
+                       
+}
+superbowlWin(record)
